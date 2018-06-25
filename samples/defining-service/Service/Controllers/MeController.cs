89bd5cf5576cabc402d27e9db5e9081fd54ab518
Oleg Karasik <OlegKarasik@coherentsolutions.com>
2018-06-25 11:36:17 +0300

    Renamed sample directory.

---
+++ samples/defining-service/Service/Controllers/MeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Controllers
+{
+    [Route("api/me")]
+    public class MeController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return $"Hello! I am running inside Service Fabric!";
+        }
+    }
+}
