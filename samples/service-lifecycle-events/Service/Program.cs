627e4286d942908d973a34ffa220a6d4817e1bae
Oleg Karasik <OlegKarasik@coherentsolutions.com>
2018-09-03 15:14:37 +0300

    Implemented wireframe for services life-cycle sample.

---
+++ samples/service-lifecycle-events/Service/Program.cs
@@ -0,0 +1,54 @@
+﻿using CoherentSolutions.Extensions.Hosting.ServiceFabric;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Service
+{
+    public class Program
+    {
+        public static void Main(
+            string[] args)
+        {
+            new HostBuilder()
+               .DefineStatefulService(
+                    serviceBuilder =>
+                    {
+                        serviceBuilder
+                           .UseServiceType("StatefulServiceType")
+                           .DefineAspNetCoreListener(
+                                listenerBuilder =>
+                                {
+                                    listenerBuilder
+                                       .UseEndpoint("StatefulServiceEndpoint")
+                                       .UseUniqueServiceUrlIntegration()
+                                       .ConfigureWebHost(
+                                            webHostBuilder =>
+                                            {
+                                                webHostBuilder.UseStartup<Startup>();
+                                            });
+                                });
+                    })
+               .DefineStatelessService(
+                    serviceBuilder =>
+                    {
+                        serviceBuilder
+                           .UseServiceType("StatelessServiceType")
+                           .DefineAspNetCoreListener(
+                                listenerBuilder =>
+                                {
+                                    listenerBuilder
+                                       .UseEndpoint("StatelessServiceEndpoint")
+                                       .UseUniqueServiceUrlIntegration()
+                                       .ConfigureWebHost(
+                                            webHostBuilder =>
+                                            {
+                                                webHostBuilder.UseStartup<Startup>();
+                                            });
+                                });
+                    })
+               .Build()
+               .Run();
+        }
+    }
+}
